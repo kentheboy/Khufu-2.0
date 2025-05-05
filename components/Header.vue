9cb27a9038810b9115b7f5349a07e7bbc22e43c4
kentheboy <foryoukentheboy1020@gmail.com>
2025-05-05 18:02:12 +0800

    add header by adding tailwind angain and NuxtUi menu component

---
+++ components/Header.vue
@@ -0,0 +1,36 @@
+<script setup>
+const { t } = useI18n();
+const items = [
+  {
+    label: t('greetings.hello'),
+    icon: 'i-lucide-book-open',
+    command: () => {
+      console.log('hello');
+    },
+  },
+  {
+    label: t('greetings.sorry'),
+    icon: 'i-lucide-database',
+    command: () => {
+      console.log('sorry');
+    },
+  },
+];
+</script>
+<template>
+  <header class="header">
+    <div class="headerChild flex justify-between h-full">
+      <a class="block" href="/">
+        <img class="logo" alt="" src="/images/main_logo.png" />
+      </a>
+      <UNavigationMenu :items="items" class="justify-end" />
+    </div>
+  </header>
+</template>
+<style scoped lang="scss">
+.header {
+    height: 7.6rem;
+    background-color: var(--color-skyblue);
+    overflow: hidden;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,5 @@
-<script setup>
-const { t } = useI18n()
-</script>
 <template>
-    <div>
-        Hello Wrld! Khfu 2.0 Here it comes!
+    <div class="home">
+
     </div>
-    <div class="p-6 bg-blue-500 text-white rounded">
-        Hello from Tailwind toge!
-    </div>
-    <p>{{ $t('greetings.hello') }}</p>
 </template>
