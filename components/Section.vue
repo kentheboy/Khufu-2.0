e7fb0dc3b65b6b54d556e42c8faebde3162d2340
kentheboy <foryoukentheboy1020@gmail.com>
2025-05-11 22:41:16 +0800

    add Section with calendar input

---
+++ components/Section.vue
@@ -0,0 +1,20 @@
+<script setup>
+const props = defineProps({
+    title: String,
+    subtitle: String,
+})
+</script>
+<template>
+    <UContainer class="h-32 mb-24 mx-auto text-center">
+        <h1 class="text-3xl font-bold text-gray-800 dark:text-white">
+            {{ title }}
+        </h1>
+        <h2 class="text-xl text-gray-600 dark:text-gray-400 pb-3">
+            {{ subtitle }}
+        </h2>
+        <hr>
+        <UContainer class="my-18 mx-auto text-center">
+            <slot />
+        </UContainer>
+    </UContainer>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,7 @@
 <script setup>
+const { t } = useI18n();
+import { CalendarDate } from '@internationalized/date'
+
 const imageImports = import.meta.glob("@/public/images/hero/*.{jpg,png}", {
   eager: true,
   import: "default",
@@ -8,10 +11,27 @@ const replaceInArray = (arr, searchValue, replaceValue) => {
 };
 
 const images = replaceInArray(Object.values(imageImports), "/_nuxt/public", "");
+
+let date = ref([
+  {
+    start: null,
+    end: null,
+  },
+]);
+const getTomorrowDate = () => {
+  const today = new Date()
+  const tomorrow = new Date(today)
+  tomorrow.setDate(today.getDate() + 1)
+  return tomorrow
+}
+const tomorrowDate = getTomorrowDate()
+const minDate = new CalendarDate(tomorrowDate.getFullYear(), tomorrowDate.getMonth() + 1, tomorrowDate.getDate());
+console.log(minDate);
+
 </script>
 <template>
   <div class="home">
-    <UContainer>
+    <UContainer class="mb-24 mx-auto">
       <UCarousel
         v-slot="{ item }"
         loop
@@ -24,5 +44,9 @@ const images = replaceInArray(Object.values(imageImports), "/_nuxt/public", "");
         <img :src="item" class="max-h-full max-w-full object-contain rounded-lg" />
       </UCarousel>
     </UContainer>
+    <Section title="SCHEDULE" :subtitle="t('home.Search by dates')">
+      <UCalendar range v-model="date" :min-value="minDate"/>
+      <!-- <UCalendar range v-model="date"/> -->
+    </Section>
   </div>
 </template>
