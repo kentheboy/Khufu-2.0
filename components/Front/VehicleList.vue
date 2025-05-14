07632a25ef7df1eaba34be1775ed3186073314b1
kentheboy <foryoukentheboy1020@gmail.com>
2025-05-14 19:47:17 +0800

    apply formatter

--- components/Front/VehicleList.vue
+++ components/Front/VehicleList.vue
@@ -5,11 +5,13 @@ const { t } = useI18n();
 </script>
 
 <template>
-  <UContainer class="my-18 text-center max-w-5xl mx-auto flex justify-center gap-4"> 
-      <VehicleCard />
-      <VehicleCard />
-      <VehicleCard />
-      <VehicleCard />
+  <UContainer
+    class="my-18 text-center max-w-5xl mx-auto flex justify-center gap-4"
+  >
+    <VehicleCard />
+    <VehicleCard />
+    <VehicleCard />
+    <VehicleCard />
   </UContainer>
 </template>
 
