defac20f7a6bf2044b7385e691742e62e2052120
kentheboy <foryoukentheboy1020@gmail.com>
2025-05-15 22:46:05 +0800

    refactor Footer component layout and restore it in the default layout

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,52 +1,67 @@
 <script setup>
+const { t } = useI18n();
 </script>
 <template>
-  <footer class="footer w-max">
+  <footer class="footer w-screen">
     <div
-      class="footerChild flex justify-between h-full items-center w-9/10 mx-auto"
+      class="footerChild flex flex-row flex-wrap justify-between h-full items-center w-9/10 mx-auto pt-10 pb-10"
     >
-      <div class="sites">
-        <div class="logo-container">
-          <img alt="" src="/images/main_logo_sm.png" />
-        </div>
-        <div>
-          <a href="/company">{{ $t("home.Company info") }}</a>
-          <a href="/terms">{{ $t("home.Terms and Conditions of Lease") }}</a>
-          <a href="/files/privacy_policy.pdf" target="_blank"
-            >{{ $t("home.Privacy Policy") }} <i class="pi pi-external-link"></i
-          ></a>
-          <a href="https://column.class-rental-car.com/" target="_blank"
-            >{{ $t("home.Articles") }} <i class="pi pi-external-link"></i
-          ></a>
-        </div>
+      <a class="block basis-1/3 mb-5" href="/">
+        <img class="logo" alt="" src="/images/main_logo_sm.png" />
+      </a>
+      <div class="basis-2/3">
+        <ul class="flex gap-4 justify-end">
+          <li>
+            <ULink as="button" class="inline-flex items-center gap-1.5 contrast">
+              <UIcon name="i-lucide-book-open" class="size-4" />
+              {{ t("home.Company info") }}
+            </ULink>
+          </li>
+          <li>
+            <ULink as="button" class="inline-flex items-center gap-1.5 contrast">
+              <UIcon name="i-lucide-book-open" class="size-4" />
+              {{ t("home.Terms and Conditions of Lease") }}
+            </ULink>
+          </li>
+          <li>
+            <ULink as="button" class="inline-flex items-center gap-1.5 contrast">
+              <UIcon name="i-lucide-book-open" class="size-4" />
+              {{ t("home.Privacy Policy") }}
+            </ULink>
+          </li>
+          <li>
+            <ULink as="button" class="inline-flex items-center gap-1.5 contrast">
+              <UIcon name="i-lucide-book-open" class="size-4" />
+              {{ t("home.Articles") }}
+            </ULink>
+          </li>
+        </ul>
       </div>
-      <div class="footer__content--postal-code">
-        {{ $t("home.67 Nagado, Toyomigusuku City, Okinawa 901-0203, Japan") }}
+      <div class="basis-1/3 flex flex-col justify-end">
+        <ULink as="button" class="inline-flex items-center gap-1.5 contrast">
+          {{ t("home.67 Nagado, Toyomigusuku City, Okinawa 901-0203, Japan") }}
+        </ULink>
+        <ULink as="button" class="inline-flex items-center gap-1.5 contrast">
+          {{ t("home.Business hours") }} 09:00-18:00
+        </ULink>
+        <ULink as="button" class="inline-flex items-center gap-1.5 contrast">
+          <UIcon name="i-lucide-phone-call" class="size-4" />
+          098-996-7266
+        </ULink>
       </div>
-      <div class="footer__content--contact">
-        <div class="sns-links">
-          <a
-            class="instagram"
-            href="https://www.instagram.com/class_rentalcar"
-            target="_blank"
-          >
-            <i class="pi pi-instagram"></i>
-          </a>
-          <a class="line" href="https://lin.ee/w5vv7ng" target="_blank">
-            <img src="/images/icons/LINE_Brand_icon.png" alt="" class="h-32px"/>
-          </a>
-        </div>
-        <div class="hours">
-          {{ $t("home.Business hours") }} 09:00-18:00
-        </div>
-        <a href="tel:098-996-7266">098-996-7266</a>
+      <div class="basis-2/3 flex justify-end">
+        <ULink as="button" class="inline-flex items-center gap-1.5 contrast">
+          <UIcon name="i-simple-icons:instagram" class="size-4" />
+        </ULink>
+        <ULink as="button" class="inline-flex items-center gap-1.5 contrast pl-3">
+          <UIcon name="i-simple-icons:line" class="size-5" />
+        </ULink>
       </div>
     </div>
   </footer>
 </template>
 <style lang="scss">
 .footer {
-  height: 7.6rem;
   background-color: var(--ui-primary);
 }
 </style>
