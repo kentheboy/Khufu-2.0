8c4f523875eb9d0196eafad73d263647dcb34bf9
kentheboy <foryoukentheboy1020@gmail.com>
2025-05-24 22:18:25 +0800

    Refactor FeatureCard component structure and styles for improved readability and responsiveness

--- components/Front/FeatureCard.vue
+++ components/Front/FeatureCard.vue
@@ -22,17 +22,20 @@ const { isVisible } = useFadeInOnScroll(containerRef, props.enableFadeIn);
       isVisible ? 'opacity-100' : 'opacity-0 pointer-events-none',
     ]"
   >
-    <UCard variant="soft" class="rounded-full feature-card flex items-center justify-center m-auto">
+    <UCard
+      variant="soft"
+      class="rounded-full feature-card flex items-center justify-center m-auto"
+    >
       <h1
         v-if="mainStatement"
         class="text-3xl font-bold text-gray-800 dark:text-white"
         v-html="mainStatement"
       ></h1>
     </UCard>
-    <div class="feature-number">{{number}}</div>
+    <h2 class="feature-number">{{ number }}</h2>
     <h3
       v-if="description"
-      class="text-2xl text-gray-600 dark:text-gray-400 pb-3 mt-16 mb-16 mx-0"
+      class="text-2xl text-gray-600 dark:text-gray-400 pb-3 mt-10 mb-16 mx-0"
       v-html="description"
     ></h3>
   </div>
@@ -51,60 +54,54 @@ const { isVisible } = useFadeInOnScroll(containerRef, props.enableFadeIn);
   }
   .feature-number {
     position: absolute;
-    top: 35px;
-    width: 57vw;
-    height: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: end;
     font-size: 10rem;
     color: var(--font-contrast-style);
     font-weight: 500;
-    text-shadow: 4.46875px 8.9375px 8.94px rgba(0, 0, 0, .5);
-    right: 0;
+    text-shadow: 4.46875px 8.9375px 8.94px rgba(0, 0, 0, 0.5);
+    right: 17%;
+    bottom: 8rem;
   }
-  
+
   @media screen and (max-width: 980px) {
-    .feature-card {
-      width: 24rem;
-      height: 24rem;
-    }
-    
-    .feature-number {
-      font-size: 8rem;
-      width: 50vw;
-      right: 5%;
-    }
-  }
-  
-  @media screen and (max-width: 720px) {
     .feature-card {
       width: 45vw;
       height: 45vw;
+      h1 {
+        font-size: 4vw;
+      }
     }
-    
+    h3 {
+      font-size: 3vw;
+    }
+
     .feature-number {
       font-size: 16vw;
     }
   }
-  
-  @media screen and (max-width: 480px) {
-    .feature-card {
-      width: 18rem;
-      height: 18rem;
-    }
-    
+
+  @media screen and (max-width: 750px) {
     .feature-number {
-      font-size: 6rem;
-      width: 40vw;
-      top: 20px;
-      right: 10%;
-    }
-    
-    h3 {
-      font-size: 1.2rem !important;
-      margin: 8px 0 !important;
+      right: -6vw;
     }
   }
+
+  // @media screen and (max-width: 480px) {
+  //   .feature-card {
+  //     width: 18rem;
+  //     height: 18rem;
+  //   }
+
+  //   .feature-number {
+  //     font-size: 6rem;
+  //     width: 40vw;
+  //     top: 20px;
+  //     right: 10%;
+  //   }
+
+  //   h3 {
+  //     font-size: 1.2rem !important;
+  //     margin: 8px 0 !important;
+  //   }
+  // }
 }
 </style>
